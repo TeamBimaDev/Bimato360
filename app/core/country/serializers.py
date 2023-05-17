5f1d7c06c53bc13136d42dcbf87b33047731f215
Med-Ali92 <bima.dev.maa@gmail.com>
2023-05-17 16:06:44 +0100

    correction update data

--- app/core/country/serializers.py
+++ app/core/country/serializers.py
@@ -2,14 +2,23 @@ from core.abstract.serializers import AbstractSerializer
 from core.country.models import BimaCoreCountry
 
 from core.currency.serializers import BimaCoreCurrencySerializer
+from rest_framework import serializers
+
+from core.currency.models import BimaCoreCurrency
 
 
 class BimaCoreCountrySerializer(AbstractSerializer):
-    currency = BimaCoreCurrencySerializer()
+    currency = BimaCoreCurrencySerializer(read_only=True)  # Show currency details in list view
+    currency_id = serializers.PrimaryKeyRelatedField(
+        queryset=BimaCoreCurrency.objects.all(),
+        source='currency',
+        write_only=True  # Accept currency ID in edit view
+    )
+
     class Meta:
         model = BimaCoreCountry
-        fields = [\
-            'id', 'name', 'code', 'address_format', 'address_view_id', 'phone_code', \
-            'name_position', 'vat_label', 'state_required', 'zip_required', 'currency', \
-            'created', 'updated' \
-            ]
+        fields = [
+            'id', 'name', 'code', 'address_format', 'address_view_id', 'phone_code',
+            'name_position', 'vat_label', 'state_required', 'zip_required', 'currency',
+            'currency_id', 'created', 'updated'
+        ]
--- app/core/country/views.py
+++ app/core/country/views.py
@@ -2,12 +2,27 @@ from core.abstract.views import AbstractViewSet
 from core.country.models import BimaCoreCountry
 from core.permissions import IsAdminOrReadOnly
 from core.country.serializers import BimaCoreCountrySerializer
+from django.shortcuts import get_object_or_404
+
+from core.currency.models import BimaCoreCurrency
 
 
 class BimaCoreCountryViewSet(AbstractViewSet):
     queryset = BimaCoreCountry.objects.all()
     serializer_class = BimaCoreCountrySerializer
     permission_classes = []
+
+    def create(self, request):
+        print(f" test loool")
+        currency_id = self.request.data.get('currency_id')
+        currency = get_object_or_404(BimaCoreCurrency, id=currency_id)
+        serializer.save(currency=currency)
+
+    def update(self, request, pk=None):
+        currency_public_id = self.request.data.get('currency')
+        currency = get_object_or_404(BimaCoreCurrency, public_id=currency_public_id)
+        request.data.currency = currency
+        super().update(self,request)
     def get_object(self):
         obj = BimaCoreCountry.objects.get_object_by_public_id(self.kwargs['pk'])
         #self.check_object_permissions(self.request, obj)
