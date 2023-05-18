a7112f0e9ccee7a66b71c675aee07a100b9cdbcc
Med-Ali92 <bima.dev.maa@gmail.com>
2023-05-18 09:38:18 +0100

    correction migrations
    correction migrations

--- app/core/country/views.py
+++ app/core/country/views.py
@@ -3,8 +3,11 @@ from core.country.models import BimaCoreCountry
 from core.permissions import IsAdminOrReadOnly
 from core.country.serializers import BimaCoreCountrySerializer
 from django.shortcuts import get_object_or_404
+from django.db.models.query import prefetch_related_objects
+from rest_framework.response import Response
 
 from core.currency.models import BimaCoreCurrency
+
 
 
 class BimaCoreCountryViewSet(AbstractViewSet):
@@ -18,11 +21,24 @@ class BimaCoreCountryViewSet(AbstractViewSet):
         currency = get_object_or_404(BimaCoreCurrency, id=currency_id)
         serializer.save(currency=currency)
 
-    def update(self, request, pk=None):
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data_to_save = request.data
         currency_public_id = self.request.data.get('currency')
         currency = get_object_or_404(BimaCoreCurrency, public_id=currency_public_id)
-        request.data.currency = currency
-        super().update(self,request)
+        data_to_save['currency_id'] = currency.id
+        serializer = self.get_serializer(instance, data=data_to_save, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        queryset = self.filter_queryset(self.get_queryset())
+        if queryset._prefetch_related_lookups:
+            instance._prefetched_objects_cache = {}
+            prefetch_related_objects([instance], *queryset._prefetch_related_lookups)
+        return Response(serializer.data)
+
+
     def get_object(self):
         obj = BimaCoreCountry.objects.get_object_by_public_id(self.kwargs['pk'])
         #self.check_object_permissions(self.request, obj)
