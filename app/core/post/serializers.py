b6b5721526642d4727861ca853bcda142ab27933
MohamedKhayriRahmani <bima.tn.dev.rmk@gmail.com>
2023-05-24 10:31:33 +0100

    Mise à jour
    Mise à jour

--- app/core/post/serializers.py
+++ app/core/post/serializers.py
@@ -1,17 +1,21 @@
 from core.abstract.serializers import AbstractSerializer
 from .models import BimaCorePost
-from core.department.serializers import BimaCoreDepartmentSerializer
 from rest_framework import serializers
 from core.department.models import BimaCoreDepartment
 
 
 class BimaCorePostSerializer(AbstractSerializer):
-    department = BimaCoreDepartmentSerializer(read_only=True)
+    department = serializers.SerializerMethodField(read_only=True)
     department_id = serializers.PrimaryKeyRelatedField(
         queryset=BimaCoreDepartment.objects.all(),
         source='department',
         write_only=True
     )
+    def get_department(self, obj):
+        return {
+            'id': obj.department.public_id.hex,
+            'name': obj.department.name,
+        }
     class Meta:
         model = BimaCorePost
         fields = [
