654ffb3965ebd2c84a8d6962cae370bd0e525de5
radhoinemejaibimatech <mradhoine@bimatech.io>
2023-06-15 10:45:20 +0100

    [skip ci]Correction calculation stock

--- app/erp/sale_document/signals.py
+++ app/erp/sale_document/signals.py
@@ -1,15 +1,19 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import BimaErpSaleDocument
 
 
-@receiver(post_save, sender=BimaErpSaleDocument)
+@receiver(pre_save, sender=BimaErpSaleDocument)
 def update_product_quantities(sender, instance, **kwargs):
-    previous_instance = sender.objects.get(id=instance.id)
+    try:
+        previous_instance = sender.objects.get(id=instance.id)
+    except ObjectDoesNotExist:
+        previous_instance = None
 
     def adjust_product_quantity(sale_document, operation):
-        for sale_document_product in sale_document.sale_document_products.all():
+        for sale_document_product in sale_document.bimaerpsaledocumentproduct_set.all():
             product = sale_document_product.product
             if sale_document.type.lower() == 'quote':
                 product.virtual_quantity = operation(product.virtual_quantity, sale_document_product.quantity)
@@ -18,12 +22,16 @@ def update_product_quantities(sender, instance, **kwargs):
                 product.quantity = operation(product.quantity, sale_document_product.quantity)
             product.save()
 
-    # If status was "Confirmed" and then changed to "Canceled" or "Draft"
-    if previous_instance.status.lower() == 'confirmed' and instance.status in ['canceled', 'draft']:
-        adjust_product_quantity(instance, lambda x, y: x + y)  # Add the product quantity
+    if previous_instance:
+        # If status was "Confirmed" and then changed to "Canceled" or "Draft"
+        if previous_instance.status.lower() == 'confirmed' and instance.status.lower() in ['canceled', 'draft']:
+            adjust_product_quantity(instance, lambda x, y: x + y)  # Add the product quantity
 
-    # If status is "Confirmed"
-    elif instance.status.lower() == 'confirmed':
-        adjust_product_quantity(instance, lambda x, y: x - y)  # Subtract the product quantity
+        # If status was "Canceled" or "Draft" and then changed to "Confirmed"
+        elif previous_instance.status.lower() in ['canceled', 'draft'] and instance.status.lower() == 'confirmed':
+            adjust_product_quantity(instance, lambda x, y: x - y)  # Subtract the product quantity
 
-
+    else:  # if it's a new instance
+        # If status is "Confirmed"
+        if instance.status.lower() == 'confirmed':
+            adjust_product_quantity(instance, lambda x, y: x - y)  # Subtract the product quantity
