23335492473caed5d59395bc968fe4f10374bd65
radhoinemejaibimatech <mradhoine@bimatech.io>
2023-06-21 09:03:05 +0100

    [skip ci] country and state inserting data

--- app/core/country/views.py
+++ app/core/country/views.py
@@ -45,6 +45,8 @@ class BimaCoreCountryViewSet(AbstractViewSet):
         generate_pdf = GeneratePdf(response, pdf, pdf_table)
         return generate_pdf.generate()
 
+
+
     @transaction.atomic
     @action(detail=False, methods=['get'], url_path='populate_database_from_csv')
     def populate_database_from_csv(self, request):
@@ -60,7 +62,7 @@ class BimaCoreCountryViewSet(AbstractViewSet):
         print("Reading countries from CSV...")
         self.process_csv(
             url='https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/csv/countries.csv',
-            processor=self.insert_country,
+            processor=self.insert_countries,
             errors=errors
         )
 
@@ -68,7 +70,7 @@ class BimaCoreCountryViewSet(AbstractViewSet):
         print("Reading states from CSV...")
         self.process_csv(
             url='https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/csv/states.csv',
-            processor=self.insert_state,
+            processor=self.insert_states,
             errors=errors
         )
 
@@ -84,50 +86,61 @@ class BimaCoreCountryViewSet(AbstractViewSet):
             csv_content = response.content.decode('utf-8').splitlines()
             reader = csv.DictReader(csv_content)
 
-            for row in reader:
-                processor(row, errors)
+            processor(reader, errors)
 
         except Exception as e:
             errors.append(str(e))
 
-    def insert_country(self, row, errors):
+    def insert_countries(self, reader, errors):
+        country_list = []
         try:
-            print(f"Inserting country: {row['name']}...")
-            currency = BimaCoreCurrency.objects.filter(currency_unit_label=row['currency']).first()
-            if not currency:
-                currency = BimaCoreCurrency.objects.first()
+            for row in reader:
+                print(f"Preparing country: {row['name']} for insertion...")
+                currency = BimaCoreCurrency.objects.filter(currency_unit_label=row['currency']).first()
+                if not currency:
+                    currency = BimaCoreCurrency.objects.first()
 
-            BimaCoreCountry.objects.create(
-                name=row['name'],
-                code=row['iso2'],
-                iso3=row['iso3'],
-                iso2=row['iso2'],
-                phone_code=row['phone_code'],
-                capital=row['capital'],
-                address_format='',
-                vat_label='',
-                zip_required=False,
-                currency=currency
-            )
+                country = BimaCoreCountry(
+                    name=row['name'],
+                    code=row['iso2'],
+                    iso3=row['iso3'],
+                    iso2=row['iso2'],
+                    phone_code=row['phone_code'],
+                    capital=row['capital'],
+                    address_format='',
+                    vat_label='',
+                    zip_required=False,
+                    currency=currency
+                )
+                country_list.append(country)
+
+            print("Bulk inserting countries...")
+            BimaCoreCountry.objects.bulk_create(country_list)
 
         except Exception as e:
-            errors.append(f"Error inserting country {row['name']}: {str(e)}")
+            errors.append(f"Error preparing countries for bulk insertion: {str(e)}")
 
-    def insert_state(self, row, errors):
+    def insert_states(self, reader, errors):
+        state_list = []
         try:
-            print(f"Inserting state: {row['name']}...")
-            country = BimaCoreCountry.objects.filter(iso2=row['country_code']).first()
-            if not country:
-                country = BimaCoreCountry.objects.first()
+            for row in reader:
+                print(f"Preparing state: {row['name']} for insertion...")
+                country = BimaCoreCountry.objects.filter(iso2=row['country_code']).first()
+                if not country:
+                    country = BimaCoreCountry.objects.first()
 
-            BimaCoreState.objects.create(
-                name=row['name'],
-                code=row['state_code'],
-                country=country
-            )
+                state = BimaCoreState(
+                    name=row['name'],
+                    code=row['state_code'],
+                    country=country
+                )
+                state_list.append(state)
+
+            print("Bulk inserting states...")
+            BimaCoreState.objects.bulk_create(state_list)
 
         except Exception as e:
-            errors.append(f"Error inserting state {row['name']}: {str(e)}")
+            errors.append(f"Error preparing states for bulk insertion: {str(e)}")
 
 
 def create_response():
