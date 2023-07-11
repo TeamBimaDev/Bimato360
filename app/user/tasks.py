69e29a3097e458c30187f178de6f7168c284453a
radhoinemejaibimatech <mradhoine@bimatech.io>
2023-07-11 08:55:27 +0100

    [skip ci] correction

--- app/user/tasks.py
+++ app/user/tasks.py
@@ -3,5 +3,5 @@ from celery import shared_task
 
 
 @shared_task(max_retries=3)
-def send_email_async(mail_subject, message, email):
-    EmailService.send_email.delay(mail_subject, message, email, html_message=None)
+def send_email_async(mail_subject, message, email, html_message=None):
+    EmailService.send_email.delay(mail_subject, message, email, html_message)
