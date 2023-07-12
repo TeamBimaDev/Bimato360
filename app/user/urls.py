0ae9ba61442319ad0029a381650112ca567f882d
radhoinemejaibimatech <mradhoine@bimatech.io>
2023-07-12 13:39:01 +0100

    improve user change password after creation

--- app/user/urls.py
+++ app/user/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
     path('reset-password/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('activate_user_account/<str:user_id>/', UserActivationView.as_view(), name='activate_user_account'),
-    path('user_activate_and_change_password/<uidb64>/<token>/', CreateUserPasswordForFirstTime.as_view(),
-         name='user_activate_and_change_password'),
+    path('user_activate_and_change_password/<str:public_id>/<str:uidb64>/<str:token>/',
+         CreateUserPasswordForFirstTime.as_view(), name='user_activate_and_change_password'),
+
     path('', include(router.urls)),
 ]
