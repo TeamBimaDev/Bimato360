34162f9ecf274bb9c622a595967f2a2678437f92
radhoinemejaibimatech <mradhoine@bimatech.io>
2023-08-21 10:38:34 +0100

    update transaction type

--- app/common/enums/transaction_enum.py
+++ app/common/enums/transaction_enum.py
@@ -3,25 +3,6 @@ from enum import Enum
 from django.utils.translation import gettext_lazy as _
 
 
-class TransactionType(Enum):
-    INCOME = _("Income")
-    EXPENSE = _("Expense")
-
-
-def get_transaction_types():
-    return [(tp.name, tp.value) for tp in TransactionType]
-
-
-class PaymentTermDetailType(Enum):
-    PERCENT = _('Percentage')
-    FIXED = _('Fixed Amount')
-    BALANCE = _("Balance")
-
-
-def get_payment_term_detail_types():
-    return [(ptd.name, ptd.value) for ptd in PaymentTermDetailType]
-
-
 class TransactionTypeIncomeOutcome(Enum):
     INCOME = _('INCOME')
     OUTCOME = _('OUTCOME')
@@ -32,8 +13,8 @@ def get_transaction_type_income_outcome():
 
 
 class TransactionTypeForCashOrBank(Enum):
-    Cash = _('Cash')
-    Bank = _('Bank')
+    CASH = _('Cash')
+    BANK = _('Bank')
 
 
 def get_transaction_type_for_cash_or_bank():
