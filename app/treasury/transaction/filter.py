88e570a965240b967df12aa53b0ba5f1ff251850
radhoinemejaibimatech <mradhoine@bimatech.io>
2023-08-24 13:54:48 +0100

    update filter

--- app/treasury/transaction/filter.py
+++ app/treasury/transaction/filter.py
@@ -1,18 +1,23 @@
 import django_filters
 from common.enums.transaction_enum import TransactionNature, TransactionDirection
+from django.db import models
 
 from .models import BimaTreasuryTransaction
 
 
 class BimaTreasuryTransactionFilter(django_filters.FilterSet):
-    nature = django_filters.CharFilter(method='filter_nature')
-    direction = django_filters.CharFilter(method='filter_direction')
-    transaction_type = django_filters.UUIDFilter(field_name="transaction_type__public_id")
-    cash = django_filters.UUIDFilter(field_name='cash__public_id')
-    bank_account = django_filters.UUIDFilter(field_name='bank_account__public_id')
-    partner = django_filters.UUIDFilter(field_name='partner__public_id')
-    date_from = django_filters.DateFilter(field_name="date", lookup_expr='gte')
-    date_to = django_filters.DateFilter(field_name="date", lookup_expr='lte')
+    nature = django_filters.CharFilter(method="filter_nature")
+    direction = django_filters.CharFilter(method="filter_direction")
+    transaction_type = django_filters.UUIDFilter(
+        field_name="transaction_type__public_id"
+    )
+    cash = django_filters.UUIDFilter(field_name="cash__public_id")
+    bank_account = django_filters.UUIDFilter(field_name="bank_account__public_id")
+    partner = django_filters.UUIDFilter(field_name="partner__public_id")
+    date_from = django_filters.DateFilter(field_name="date", lookup_expr="gte")
+    date_to = django_filters.DateFilter(field_name="date", lookup_expr="lte")
+    search = django_filters.CharFilter(method="filter_search")
+    year = django_filters.NumberFilter(field_name="date", lookup_expr="year")
 
     class Meta:
         model = BimaTreasuryTransaction
@@ -29,3 +34,10 @@ class BimaTreasuryTransactionFilter(django_filters.FilterSet):
             return queryset
         direction_value = TransactionDirection.get_name(value).upper()
         return queryset.filter(direction=direction_value)
+
+    def filter_search(self, queryset, name, value):
+        return queryset.filter(
+            models.Q(note__icontains=value)
+            | models.Q(reference__icontains=value)
+            | models.Q(partner_bank_account_number__icontains=value)
+        )
--- app/treasury/transaction/views.py
+++ app/treasury/transaction/views.py
@@ -1,6 +1,10 @@
 from common.permissions.action_base_permission import ActionBasedPermission
 from core.abstract.views import AbstractViewSet
-from core.entity_tag.models import get_entity_tags_for_parent_entity, create_single_entity_tag, BimaCoreEntityTag
+from core.entity_tag.models import (
+    get_entity_tags_for_parent_entity,
+    create_single_entity_tag,
+    BimaCoreEntityTag,
+)
 from core.entity_tag.serializers import BimaCoreEntityTagSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
@@ -18,24 +22,31 @@ class BimaTreasuryTransactionViewSet(AbstractViewSet):
     serializer_class = BimaTreasuryTransactionSerializer
     permission_classes = []
     permission_classes = (ActionBasedPermission,)
-    ordering_fields = ['nature', 'direction', 'transaction_type', 'partner__name', 'amount', 'date']
-    ordering = ['date']
+    ordering_fields = [
+        "nature",
+        "direction",
+        "transaction_type",
+        "partner__name",
+        "amount",
+        "date",
+    ]
+    ordering = ["date"]
     filterset_class = BimaTreasuryTransactionFilter
     action_permissions = {
-        'list': ['transaction.can_read'],
-        'create': ['transaction.can_create'],
-        'retrieve': ['transaction.can_read'],
-        'update': ['transaction.can_update'],
-        'partial_update': ['transaction.can_update'],
-        'destroy': ['transaction.can_delete'],
-        'get_history_diff': ['transaction.can_view_history'],
+        "list": ["transaction.can_read"],
+        "create": ["transaction.can_create"],
+        "retrieve": ["transaction.can_read"],
+        "update": ["transaction.can_update"],
+        "partial_update": ["transaction.can_update"],
+        "destroy": ["transaction.can_delete"],
+        "get_history_diff": ["transaction.can_view_history"],
     }
 
     def get_object(self):
-        obj = BimaTreasuryTransaction.objects.get_object_by_public_id(self.kwargs['pk'])
+        obj = BimaTreasuryTransaction.objects.get_object_by_public_id(self.kwargs["pk"])
         return obj
 
-    @action(detail=True, methods=['GET'], url_path='get_transaction_history')
+    @action(detail=True, methods=["GET"], url_path="get_transaction_history")
     def get_transaction_history(self, request, pk=None):
         transaction = self.get_object()
         history = transaction.history.all()
@@ -45,49 +56,73 @@ class BimaTreasuryTransactionViewSet(AbstractViewSet):
         serialized_history = TransactionHistorySerializer(history, many=True)
         return Response(serialized_history.data)
 
-    @action(detail=False, methods=['GET'], url_path='transaction_sums')
+    @action(detail=False, methods=["GET"], url_path="transaction_sums")
     def transaction_sums(self, request):
-        filtered_qs = BimaTreasuryTransactionFilter(request.GET, queryset=BimaTreasuryTransaction.objects.all()).qs
+        filtered_qs = BimaTreasuryTransactionFilter(
+            request.GET, queryset=BimaTreasuryTransaction.objects.all()
+        ).qs
         service = BimaTreasuryTransactionService(filtered_qs)
         result = service.calculate_sums()
         return Response(result)
 
-    @action(detail=False, methods=['GET'], url_path='export_to_csv')
+    @action(detail=False, methods=["GET"], url_path="export_to_csv")
     def export_to_csv(self, request):
-        filtered_qs = BimaTreasuryTransactionFilter(request.GET, queryset=BimaTreasuryTransaction.objects.all()).qs
+        filtered_qs = BimaTreasuryTransactionFilter(
+            request.GET, queryset=BimaTreasuryTransaction.objects.all()
+        ).qs
         service = BimaTreasuryTransactionService(filtered_qs)
         file_path = service.export_to_csv("transactions_export.csv")
         return Response({"file_path": file_path})
 
-    @action(detail=False, methods=['GET'], url_path='export_to_excel')
+    @action(detail=False, methods=["GET"], url_path="export_to_excel")
     def export_to_excel(self, request):
-        filtered_qs = BimaTreasuryTransactionFilter(request.GET, queryset=BimaTreasuryTransaction.objects.all()).qs
+        filtered_qs = BimaTreasuryTransactionFilter(
+            request.GET, queryset=BimaTreasuryTransaction.objects.all()
+        ).qs
         service = BimaTreasuryTransactionService(filtered_qs)
         file_path = service.export_to_excel("transactions_export.xlsx")
         return Response({"file_path": file_path})
 
     def list_tags(self, request, *args, **kwargs):
-        transaction = BimaTreasuryTransaction.objects.get_object_by_public_id(self.kwargs['public_id'])
-        entity_tags = get_entity_tags_for_parent_entity(transaction).order_by('order')
+        transaction = BimaTreasuryTransaction.objects.get_object_by_public_id(
+            self.kwargs["public_id"]
+        )
+        entity_tags = get_entity_tags_for_parent_entity(transaction).order_by("order")
         serialized_entity_tags = BimaCoreEntityTagSerializer(entity_tags, many=True)
         return Response(serialized_entity_tags.data)
 
     def create_tag(self, request, *args, **kwargs):
-        transaction = BimaTreasuryTransaction.objects.get_object_by_public_id(self.kwargs['public_id'])
+        transaction = BimaTreasuryTransaction.objects.get_object_by_public_id(
+            self.kwargs["public_id"]
+        )
         result = create_single_entity_tag(request.data, transaction)
         if isinstance(result, BimaCoreEntityTag):
             serialized_entity_tags = BimaCoreEntityTagSerializer(result)
             return Response(serialized_entity_tags.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(result, status=result.get("status", status.HTTP_500_INTERNAL_SERVER_ERROR))
+            return Response(
+                result,
+                status=result.get("status", status.HTTP_500_INTERNAL_SERVER_ERROR),
+            )
 
     def get_tag(self, request, *args, **kwargs):
-        transaction = BimaTreasuryTransaction.objects.get_object_by_public_id(self.kwargs['public_id'])
-        entity_tags = get_object_or_404(BimaCoreEntityTag,
-                                        public_id=self.kwargs['entity_tag_public_id'],
-                                        parent_id=transaction.id)
+        transaction = BimaTreasuryTransaction.objects.get_object_by_public_id(
+            self.kwargs["public_id"]
+        )
+        entity_tags = get_object_or_404(
+            BimaCoreEntityTag,
+            public_id=self.kwargs["entity_tag_public_id"],
+            parent_id=transaction.id,
+        )
         if not entity_tags:
             return Response({"error": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serialized_entity_tags = BimaCoreEntityTagSerializer(entity_tags)
         return Response(serialized_entity_tags.data)
+
+    @action(detail=False, methods=["GET"], url_path="available_transactions_years")
+    def available_transactions_years(self, request, *args, **kwargs):
+        years = BimaTreasuryTransaction.objects.dates("date", "year").values_list(
+            "date__year", flat=True
+        )
+        return Response(years)
