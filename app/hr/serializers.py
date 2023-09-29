3e163b45cfcc3dbac841acb29e64f39fc6f78d29
radhoinemejaibimatech <mradhoine@bimatech.io>
2023-09-29 10:46:41 +0100

    correction

--- app/hr/serializers.py
+++ app/hr/serializers.py
@@ -1,3 +1,4 @@
+from common.converters.default_converters import str_to_bool
 from core.abstract.serializers import AbstractSerializer
 from hr.skill.models import BimaHrSkill
 from rest_framework import serializers
@@ -24,6 +25,37 @@ class BimaHrPersonExperienceSerializer(AbstractSerializer):
         write_only=True
     )
 
+    def get_person(self, obj):
+        if obj.person:
+            return {
+                'id': obj.person.public_id.hex,
+                'name': obj.person.full_name,
+            }
+        return None
+
+    def save(self, **kwargs):
+        is_current_position = str_to_bool(self.validated_data.get('is_current_position', False))
+        if is_current_position:
+            self.validated_data['date_end'] = None
+
+        return super().save(**kwargs)
+
+    class Meta:
+        model = BimaHrPersonExperience
+        fields = [
+            'id', 'person', 'person_public_id', 'company_name', 'position', 'description',
+            'date_begin', 'date_end', 'is_current_position', 'experience_duration', 'created', 'updated'
+        ]
+
+
+class BimaHrPersonSkillSerializer(AbstractSerializer):
+    person = serializers.SerializerMethodField(read_only=True)
+    person_public_id = serializers.SlugRelatedField(
+        queryset=BimaHrPerson.objects.all(),
+        slug_field='public_id',
+        source='person',
+        write_only=True
+    )
     skill = serializers.SerializerMethodField(read_only=True)
     skill_public_id = serializers.SlugRelatedField(
         queryset=BimaHrSkill.objects.all(),
@@ -51,31 +83,6 @@ class BimaHrPersonExperienceSerializer(AbstractSerializer):
     class Meta:
         model = BimaHrPersonExperience
         fields = [
-            'id', 'person', 'person_public_id', 'skill', 'skill_public_id', 'company_name', 'position', 'description',
-            'date_begin', 'date_end', 'is_current_position', 'experience_duration', 'created', 'updated'
-        ]
-
-
-class BimaHrPersonSkillSerializer(AbstractSerializer):
-    person = serializers.SerializerMethodField(read_only=True)
-    person_public_id = serializers.SlugRelatedField(
-        queryset=BimaHrPerson.objects.all(),
-        slug_field='public_id',
-        source='person',
-        write_only=True
-    )
-
-    def get_person(self, obj):
-        if obj.person:
-            return {
-                'id': obj.person.public_id.hex,
-                'name': obj.person.full_name,
-            }
-        return None
-
-    class Meta:
-        model = BimaHrPersonExperience
-        fields = [
-            'id', 'person', 'person_public_id', 'company_name', 'description', 'date_begin',
+            'id', 'person', 'person_public_id', 'company_name', 'skill', 'skill_public_id', 'description', 'date_begin',
             'date_end', 'is_current_position', 'created', 'updated'
         ]
